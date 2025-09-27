a7527942507cdad5423a013b32fb86d7ab7e916c
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-09-26 21:02:31 -0500

    Update Host.cs

--- src/MegabonkMultiplayer/Net/Host.cs
+++ src/MegabonkMultiplayer/Net/Host.cs
@@ -1,48 +1,71 @@
+using System.Collections.Generic;
+using LiteNetLib;
+using LiteNetLib.Utils;
+using UnityEngine;
+
 namespace MegabonkMultiplayer.Net
 {
-  using LiteNetLib;
-  using LiteNetLib.Utils;
-  using System.Collections.Generic;
-  using UnityEngine;
-
   public static class Host
   {
     static readonly List<NetPeer> _peers = new();
-    static readonly EventBasedNetListener _l = NetCommon.Listener;
+    static bool _wired;
 
-    static Host()
+    static void EnsureWired()
     {
-      _l.ConnectionRequestEvent += req => req.AcceptIfKey("mbonk"); // simple key
-      _l.PeerConnectedEvent += peer => { _peers.Add(peer); };
-      _l.PeerDisconnectedEvent += (peer, info) => { _peers.Remove(peer); };
-      _l.NetworkReceiveEvent += OnReceive;
+      if (_wired) return;
+      _wired = true;
+
+      var l = NetCommon.Listener;
+
+      // Simple key to avoid random LAN connects during tests
+      l.ConnectionRequestEvent += req => req.AcceptIfKey("mbonk");
+
+      l.PeerConnectedEvent += peer =>
+      {
+        if (!_peers.Contains(peer)) _peers.Add(peer);
+      };
+
+      l.PeerDisconnectedEvent += (peer, info) =>
+      {
+        _peers.Remove(peer);
+      };
+
+      l.NetworkReceiveEvent += (peer, reader, channel, method) =>
+      {
+        var op = (OpCode)reader.GetByte();
+        switch (op)
+        {
+          case OpCode.Hello:
+            // Optionally: send initial snapshot
+            break;
+
+          // case OpCode.FireEvent:
+          //   // Apply client input/intent to the authoritative sim
+          //   break;
+        }
+        reader.Recycle();
+      };
     }
 
-    public static void Tick() => NetCommon.Poll();
-
-    static void OnReceive(NetPeer peer, NetPacketReader reader, byte channel, DeliveryMethod method)
+    public static void Tick()
     {
-      var op = (OpCode)reader.GetByte();
-      switch (op)
-      {
-        case OpCode.Hello:
-          // could ack here
-          break;
-        case OpCode.PlayerInput:
-          // TODO: apply client input to authoritative sim
-          break;
-      }
-      reader.Recycle();
+      EnsureWired();
+      NetCommon.Poll();
+      // Drive the authoritative sim here if needed
     }
 
     public static void BroadcastPlayerTransform(Vector3 pos, Quaternion rot)
     {
-      var w = NetCommon.Writer;
+      if (_peers.Count == 0) return;
+
+      NetDataWriter w = NetCommon.Writer;
       w.Reset();
       w.Put((byte)OpCode.PlayerXform);
       w.Put(pos.x); w.Put(pos.y); w.Put(pos.z);
       w.Put(rot.x); w.Put(rot.y); w.Put(rot.z); w.Put(rot.w);
-      foreach (var p in _peers) p.Send(w, DeliveryMethod.Unreliable);
+
+      foreach (var p in _peers)
+        p.Send(w, DeliveryMethod.Unreliable); // fine for frequent pose updates
     }
   }
 }
