4f0b4a0a316858a3c07297320b706a70da861378
Caleb Brendel <109250665+CalebBrendel@users.noreply.github.com>
2025-09-26 21:02:43 -0500

    Update Client.cs

--- src/MegabonkMultiplayer/Net/Client.cs
+++ src/MegabonkMultiplayer/Net/Client.cs
@@ -1,56 +1,76 @@
+using LiteNetLib;
+using LiteNetLib.Utils;
+using UnityEngine;
+
 namespace MegabonkMultiplayer.Net
 {
-  using LiteNetLib;
-  using LiteNetLib.Utils;
-  using UnityEngine;
-
   public static class Client
   {
+    static NetPeer _peer;
+    static bool _wired;
+
     static Vector3 _pos;
     static Quaternion _rot;
-    static bool _have;
-    static readonly EventBasedNetListener _l = NetCommon.Listener;
-    static NetPeer? _peer;
+    static bool _havePose;
 
-    static Client()
+    static void EnsureWired()
     {
-      _l.PeerConnectedEvent += p => {
+      if (_wired) return;
+      _wired = true;
+
+      var l = NetCommon.Listener;
+
+      l.PeerConnectedEvent += p =>
+      {
         _peer = p;
+
+        // Say hello
         var w = NetCommon.Writer; w.Reset();
-        w.Put((byte)OpCode.Hello); p.Send(w, DeliveryMethod.ReliableOrdered);
+        w.Put((byte)OpCode.Hello);
+        p.Send(w, DeliveryMethod.ReliableOrdered);
       };
-      _l.NetworkReceiveEvent += OnReceive;
+
+      l.NetworkReceiveEvent += (peer, reader, channel, method) =>
+      {
+        var op = (OpCode)reader.GetByte();
+        switch (op)
+        {
+          case OpCode.PlayerXform:
+            _pos = new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+            _rot = new Quaternion(reader.GetFloat(), reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
+            _havePose = true;
+            break;
+
+          // case OpCode.Spawn: ...
+          // case OpCode.Despawn: ...
+          // case OpCode.EnemyHP: ...
+        }
+        reader.Recycle();
+      };
     }
 
-    public static void Connect(string addr = "127.0.0.1", int port = 27015)
+    static void EnsureConnected(string addr = "127.0.0.1", int port = 27015)
     {
+      if (_peer != null && _peer.ConnectionState == ConnectionState.Connected) return;
+
       if (NetCommon.Manager.ConnectedPeerList.Count == 0)
+      {
         NetCommon.Manager.Connect(addr, port, "mbonk");
+      }
     }
 
     public static void Tick()
     {
-      if (_peer == null) Connect();
+      EnsureWired();
+      EnsureConnected();
       NetCommon.Poll();
-    }
-
-    static void OnReceive(NetPeer peer, NetPacketReader reader, byte channel, DeliveryMethod method)
-    {
-      var op = (OpCode)reader.GetByte();
-      switch (op)
-      {
-        case OpCode.PlayerXform:
-          _pos = new Vector3(reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
-          _rot = new Quaternion(reader.GetFloat(), reader.GetFloat(), reader.GetFloat(), reader.GetFloat());
-          _have = true;
-          break;
-      }
-      reader.Recycle();
     }
 
     public static bool TryGetHostTransform(out Vector3 pos, out Quaternion rot)
     {
-      pos = _pos; rot = _rot; return _have;
+      pos = _pos;
+      rot = _rot;
+      return _havePose;
     }
   }
 }
